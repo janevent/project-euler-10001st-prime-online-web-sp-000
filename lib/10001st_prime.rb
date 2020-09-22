789fd09aaebfabbe54cab3445b3324a4284b3de0
Jane Venter <janeanneventer@gmail.com>
2020-09-22 05:01:45 +0000

    Automatically backed up by Learn

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+def prime_number_for(nth_element)
+  primes_array = [2, 3, 5, 7]
+  
+  i = 11
+  while primes_array.length <= nth_element  
+    prime = true
+    primes_array.each do |item| 
+      puts item
+      if i % item == 0
+        prime = false 
+      end
+    end
+    if prime == true 
+      primes_array.push(i) 
+    end
+    i=+1
+    prime = true
+  end
+  primes_array[nth_element-1]
+end
