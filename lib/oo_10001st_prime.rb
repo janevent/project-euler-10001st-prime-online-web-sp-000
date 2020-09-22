8a2705824b38ae7d3bc25519f162bd3e9fc80b6d
Jane Venter <janeanneventer@gmail.com>
2020-09-22 05:11:49 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -6,5 +6,11 @@ class Prime
   end
   
   def number 
+    primes = [2,3]
+    i = 4
+    until primes.length = self.element 
+      primes << i if primes.all? {|n| i % n > 0 }
+    end
+    i+=1
   end
 end
